33ed4be985d26872739b1a0ec8d101d42b0a7471
jsonZhu2017 <zjs.sizhe@qq.com>
2024-08-28 14:36:45 +0800

    20240828提交

--- customer/src/main/java/json/zhu/entity/TestEntity.java
+++ customer/src/main/java/json/zhu/entity/TestEntity.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,20 +20,16 @@ import java.io.Serializable;
 @Getter
 @Setter
 @TableName("test")
-@ApiModel(value = "TestEntity对象", description = "")
 public class TestEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty("主键ID")
     @TableId(value = "id", type = IdType.ASSIGN_ID)
     private String id;
 
-    @ApiModelProperty("姓名")
     @TableField("name")
     private String name;
 
-    @ApiModelProperty("年龄")
     @TableField("age")
     private String age;
 
